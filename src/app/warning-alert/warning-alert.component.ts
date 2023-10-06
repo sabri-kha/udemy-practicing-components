596a27d3a0fe929cabbd14af2934f6b2a22fc2bc
Sabri Khaled <kha.sabri@gmail.com>
2023-10-06 10:56:18 -0400

    committing completed assignment for udemy course

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { SuccessAlertComponent } from '../success-alert/success-alert.component';
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: './warning-alert.component.html',
+  styleUrls: ['./warning-alert.component.css']
+})
+export class WarningAlertComponent {
+
+}
